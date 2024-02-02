8ca591f734ec65af205e72a1825b5bfc0450b12f
Shahzil Imran <124806351+shahzcoder@users.noreply.github.com>
2024-02-02 19:45:41 +0500

    Time: 1 ms (81.27%) | Memory: 44.9 MB (11.39%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] shuffledString = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            shuffledString[indices[i]] = s.charAt(i);
+        }
+        return new String(shuffledString);
+         
+    }
+}
