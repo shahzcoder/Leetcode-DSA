3a39a28fcf01ed9d8cb6d8c7515c3096fe4cfde0
Shahzil Imran <124806351+shahzcoder@users.noreply.github.com>
2024-02-02 22:01:14 +0500

    Time: 0 ms (100.00%) | Memory: 40.1 MB (53.90%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if (n % 2 == 0) {
+        if (n%2 == 0) {
+            n = n - 1;
             return true;
-         }
-
+        }
         return false;
     }
 }
