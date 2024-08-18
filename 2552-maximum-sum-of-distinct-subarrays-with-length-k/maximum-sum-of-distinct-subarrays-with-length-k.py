f1125bb7574d53497c7132738f0a5f119b408360
Shahzil Imran <124806351+shahzcoder@users.noreply.github.com>
2024-08-19 01:24:37 +0500

    Time: 546 ms (74.20%) | Memory: 30.9 MB (98.55%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,22 +1,25 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        max_sum = 0
+        start = 0
         curr_sum = 0
-        seen = set()
-        left = 0
-        for right in range(len(nums)):
-            while nums[right] in seen:
-                seen.remove(nums[left])
-                curr_sum -= nums[left]
-                left += 1
-            
-            seen.add(nums[right])
-            curr_sum += nums[right]
-        
-            if right - left + 1 == k:
-                max_sum = max(max_sum , curr_sum)
-                seen.remove(nums[left])
-                curr_sum -= nums[left]
-                left += 1
+        max_sum = 0
+        uniques = set()
 
+        for end, val in enumerate(nums):
+            curr_sum += val
+            while val in uniques:
+                uniques.remove(nums[start])
+                curr_sum -= nums[start]
+                start += 1
+                
+            uniques.add(val)
+
+            while end - start + 1 > k:
+                uniques.remove(nums[start])
+                curr_sum -= nums[start]
+                start += 1
+            
+            if end - start + 1 == k:
+                max_sum = max(max_sum, curr_sum)
+        
         return max_sum
