ae070d1a917c5a152452eef51feaec6abbd5caf4
Shahzil Imran <124806351+shahzcoder@users.noreply.github.com>
2024-08-20 06:08:08 +0500

    Time: 35 ms (73.29%) | Memory: 16.6 MB (21.57%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # using Dutch National Flag Algorithm (useful with 3 distinct values)
+    
+        low , mid , high = 0, 0, len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[high], nums[mid] = nums[mid], nums[high]
+                high -= 1
+            
+        return nums
