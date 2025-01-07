7b447075fbc4ff992f6bdf3e02cd367e03806c5d
Shahzil Imran <124806351+shahzcoder@users.noreply.github.com>
2025-01-07 22:08:17 +0500

    Time: 7 ms (90.82%) | Memory: 17.8 MB (38.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,15 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         count = defaultdict(int)
 
-        for i in s:
-            count[i] += 1
+        for char in s:
+            count[char] += 1
 
-        for j in t:
-            count[j] -= 1
+        for char in t:
+            count[char] -= 1
 
         for val in count.values():
             if val != 0:
                 return False
         
         return True
-
